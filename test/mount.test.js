4941cab8e6c15670fe3f2bd440b8276b64c45f84
Dave Honneffer <pearofducks@gmail.com>
2020-09-20 07:54:37 +0200

    Expand tests, cleanup

--- test/mount.test.js
+++ test/mount.test.js
@@ -1,11 +1,12 @@
 import { test } from 'uvu'
 import * as assert from 'uvu/assert'
-import { isVNode, h } from 'vue'
+import { isVNode, h, createApp } from 'vue'
 import { mount as createComponentInstance } from '../index'
 
 const createComponent = (options) => ({
   template: '<h1 class="my-class">Hello {{ name }}<slot /></h1>',
-  props: { name: String }
+  props: { name: String },
+  ...options
 })
 
 test('it can create an instance without any optional arguments', () => {
@@ -44,8 +45,44 @@ test('it can create mount into a specified DOM element', () => {
   assert.is(el.tagName, 'SECTION')
 })
 
-// mount and unmount hooks run
-// destroy
-// app context
+test('it can create create a component instance with mount hooks', () => {
+  let called = false
+  const mounted = () => called = true
+  const comp = createComponent({ mounted })
+  createComponentInstance(comp)
+  assert.ok(called)
+})
+
+test('it can create create a component instance with unmount hooks', () => {
+  let called = false
+  const unmounted = () => called = true
+  const comp = createComponent({ unmounted })
+  const { destroy } = createComponentInstance(comp)
+  assert.not(called)
+  destroy()
+  assert.ok(called)
+})
+
+test('it can remove itself from the DOM when destroyed', () => {
+  const comp = createComponent()
+  const { el, destroy } = createComponentInstance(comp)
+  assert.is(el.querySelector('h1').textContent, 'Hello ')
+  destroy()
+  assert.is(el.querySelector('h1'), null)
+})
+
+test('it lacks appContext without an app being provided', () => {
+  const comp = createComponent()
+  const { vNode } = createComponentInstance(comp)
+  assert.not(vNode.appContext)
+})
+
+test('it can append an appContext to the vNode', () => {
+  const appComponent = createComponent()
+  const comp = createComponent()
+  const app = createApp(appComponent)
+  const { vNode } = createComponentInstance(comp, { app })
+  assert.ok(vNode.appContext)
+})
 
 test.run()
